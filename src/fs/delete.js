6e25844bc380ed54f62e7dfb8f47f8861bd7e032
Konstantin Si <apelsin1987@gmail.com>
2024-01-27 22:01:55 +0100

    feat: solve tasks delete and rename

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import { access, unlink } from "fs/promises"
+import { fileURLToPath } from "url"
+import { dirname, join } from "path"
 const remove = async () => {
-    // Write your code here 
-};
+  const filePath = join(
+    dirname(fileURLToPath(import.meta.url)),
+    "files",
+    "fileToRemove.txt"
+  )
+  try {
+    await access(filePath)
+    unlink(filePath)
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      throw new Error("FS operation failed")
+    }
+  }
+}
 
-await remove();
+await remove()
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,28 @@
+import fs from "fs/promises"
+import { fileURLToPath } from "url"
+import { dirname, join } from "path"
 const rename = async () => {
-    // Write your code here 
-};
+  const dirPath = join(dirname(fileURLToPath(import.meta.url)), "files")
+  const oldFilePath = join(dirPath, "wrongFilename.txt")
+  const newFilePath = join(dirPath, "properFilename.md")
+  const errFailed = new Error("FS operation failed")
+  try {
+    await fs.access(oldFilePath)
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      throw errFailed
+    }
+  }
+  try {
+    await fs.access(newFilePath)
+    throw errFailed
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      await fs.rename(oldFilePath, newFilePath)
+    } else {
+      throw err
+    }
+  }
+}
 
-await rename();
+await rename()
