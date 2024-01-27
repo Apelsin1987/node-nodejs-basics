b2b01f5bcca78df3b90cde651009ac38d8881e43
Konstantin Si <apelsin1987@gmail.com>
2024-01-27 22:24:44 +0100

    feat: solve tasks list and read

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,25 @@
+import { access, readdir } from "fs/promises"
+import { fileURLToPath } from "url"
+import { dirname, join } from "path"
 const list = async () => {
-    // Write your code here 
-};
+  const dirPath = join(dirname(fileURLToPath(import.meta.url)), "files")
+  try {
+    await access(dirPath)
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      throw new Error("FS operation failed")
+    }
+  }
+  try {
+    const files = await readdir(dirPath, { withFileTypes: true })
+    files.forEach((file) => {
+      if (file.isFile()) {
+        console.log(file.name)
+      }
+    })
+  } catch (err) {
+    console.error(err)
+  }
+}
 
-await list();
+await list()
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,27 @@
+import { access, readFile } from "fs/promises"
+import { fileURLToPath } from "url"
+import { dirname, join } from "path"
 const read = async () => {
-    // Write your code here 
-};
+  const filePath = join(
+    dirname(fileURLToPath(import.meta.url)),
+    "files",
+    "fileToRead.txt"
+  )
+  try {
+    await access(filePath)
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      throw new Error("FS operation failed")
+    } else {
+      console.error(err)
+    }
+  }
+  try {
+    const data = await readFile(filePath)
+    console.log(data.toString())
+  } catch (err) {
+    console.error(err)
+  }
+}
 
-await read();
+await read()
