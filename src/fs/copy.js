04b5b9001fe95e36bce0472fc5aaf5d58e4968e5
Konstantin Si <apelsin1987@gmail.com>
2024-01-27 21:31:14 +0100

    feat: solve tasks copy and create

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,30 @@
-const copy = async () => {
-    // Write your code here 
-};
+import { access, cp } from "fs/promises"
+import { fileURLToPath } from "url"
+import { dirname, join } from "path"
 
-await copy();
+const copy = async () => {
+  const dirName = "files"
+  const dirPath = dirname(fileURLToPath(import.meta.url))
+  const dirFrom = join(dirPath, dirName)
+  const dirTo = join(dirPath, `${dirName}-copy`)
+  const errFailed = new Error("FS operation failed")
+  try {
+    await access(dirFrom)
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      throw errFailed
+    }
+  }
+  try {
+    await access(dirTo)
+    throw errFailed
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      await cp(dirFrom, dirTo, { recursive: true })
+    } else {
+      throw err
+    }
+  }
+}
+
+await copy()
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import { access, writeFile } from "fs/promises"
+import { fileURLToPath } from "url"
+import { dirname, join } from "path"
 const create = async () => {
-    // Write your code here 
-};
+  const filePath = join(
+    dirname(fileURLToPath(import.meta.url)),
+    "files",
+    "fresh.txt"
+  )
+  try {
+    await access(filePath)
+    throw new Error("FS operation failed")
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      await writeFile(filePath, "I am fresh and young")
+    } else {
+      throw err
+    }
+  }
+}
 
-await create();
+await create()
