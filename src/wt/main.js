1ad1e1f90848ee686b11259575e1783ada5d3caf
Konstantin Si <apelsin1987@gmail.com>
2024-01-28 15:43:59 +0100

    feat: solve worker threads tasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,24 @@
-const performCalculations = async () => {
-    // Write your code here
-};
+import { Worker } from "worker_threads"
+import { fileURLToPath } from "url"
+import { dirname, join } from "path"
+import { cpus } from "os"
 
-await performCalculations();
+const performCalculations = async () => {
+  const filePath = join(dirname(fileURLToPath(import.meta.url)), "worker.js")
+  const n = 10
+  const promisesArr = []
+
+  for (let i = 0; i < cpus().length; i += 1) {
+    const promise = new Promise((resolve) => {
+      const worker = new Worker(filePath, { workerData: i + n })
+      worker.on("message", (result) => {
+        resolve(result)
+      })
+    })
+    promisesArr.push(promise)
+  }
+
+  Promise.all(promisesArr).then((resultArr) => console.log(resultArr))
+}
+
+await performCalculations()
