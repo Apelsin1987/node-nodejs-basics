1498c83d5336c495402f6d74de61fac960c2d15d
Konstantin Si <apelsin1987@gmail.com>
2024-01-28 11:02:35 +0100

    feat: solve task calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,26 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import { createHash } from "crypto"
+import { createReadStream } from "fs"
+import { fileURLToPath } from "url"
+import { dirname, join } from "path"
 
-await calculateHash();
+const calculateHash = async () => {
+  const filePath = join(
+    dirname(fileURLToPath(import.meta.url)),
+    "files",
+    "fileToCalculateHashFor.txt"
+  )
+  const hash = createHash("sha256")
+  const rs = createReadStream(filePath)
+
+  rs.on("data", (chunk) => {
+    hash.update(chunk)
+  })
+  rs.on("end", () => {
+    console.log(hash.digest("hex"))
+  })
+  rs.on("error", (err) => {
+    console.error(err.message)
+  })
+}
+
+await calculateHash()
